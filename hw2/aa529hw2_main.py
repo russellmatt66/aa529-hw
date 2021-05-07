72146963ac9d32d1fd1066bd91bcc4a21df1b4b2
russellmatt66 <russellmatt66@gmail.com>
2021-05-07 16:31:34 -0700

    Work the units out and revisit #5

--- hw2/aa529hw2_main.py
+++ hw2/aa529hw2_main.py
@@ -21,13 +21,61 @@ LiqH2O2rxn_deltaH = 460.38 * 10**(3) * (1.0/1.6) * 10**(19) # [kJ]->[J]->[eV]
 print("The energy per reaction for liquid H2/O2 is %.2E [eV]" %LiqH2O2rxn_deltaH)
 
 """ Problem 2 """
+species = ["electrons", "xenon"]
 n0 = 1.0*10**(18) # [1/m^{3}]
 Te = 10.0 # [eV]
 B0 = 0.02 # [T]
 E = 300.0 # [V/cm]
 me = 9.11*10**(-31) # [kg]
+mass_xenon = 131 * 1.67 * 10**(-27) # [kg]
 e = 1.6*10**(-19) # [C]
+Z = 1 # ion charge state
 epsilon_0 = 8.854 * 10**(-12) # [F/m]
+
+mass_species = [me, mass_xenon]
+mu = me/mass_xenon # electron-ion mass ratio
+
+lambda_ie = 20.0 # Coulomb logarithm for ion-electron collisions
+
+# Part (a): Ion and Electron Larmor Radii
+# Part (b): Ion and Electron Cycloctron frequency
+i = 0
+for mass in mass_species:
+    r_larmor = mass*(E/(e*B0**(2)))
+    omega_c = e*B0/mass
+    print("The %s Larmor radius is %f []" $(species[i],r_larmor))
+    print("The %s cyclotron frequency is %f []" $(species[i],omega_c))
+    i += 1
+
+# Part (c): ExB drift velocity
+v_EB = (E*B0)/B0**(2) # |ExB| = |E||B|
+print("The ExB drift velocity is %f []" %v_EB)
+
+# Part (d): Debye Length
+lambda_De = 6.9*np.sqrt(Te/n0) 
+print("The Debye Length is %f [cm]" %lambda_De)
+
+# Part (e): Plasma Frequency
+v_the = np.sqrt(Te/me) #
+omega_p = v_the/lambda_De
+print("The plasma frequency is %f []" %omega_p)
+
+# Part (f): Ion-electron collision frequency
+nu_ie = n0*Z**(2)*lambda_ie*(1.6*10**(-9)*np.sqrt(mu)*Te**(-3/2)) # assume quasineutrality
+print("The ion-electron collision frequency is %f []" %nu_ie)
+
+""" Problem 3 """
+# Part (a): Symbolic math
+# Part (b): Compute components of electron conductivity tensor
+nu_e = 2.9*10**(-6)*n0*lambda*Te**(-3/2) # [1/sec]
+Omega_H = (e*B0/me)/nu_e # recomputing omega_ce due to lack of foresight
+sigma_parallel_electrons = (n0*e**(2))/(me*nu_e) # []
+sigma_perp_electrons = sigma_parallel_electrons/(1.0 + Omega_H**(2)) # []
+sigma_Pederson_electrons = sigma_perp_electrons*Omega_H # []
+# Part(c): Compare magnetized conductivities to conductivity of copper
+print("The magnetized parallel electron conductivity is %f []" %sigma_parallel_electrons)
+print("The magnetized perpendicular electron conductivity is %f []" %sigma_perp_electrons)
+print("The magnetized parallel electron conductivity is %f []" %sigma_Pederson_electrons)
 
 """ Problem 4 """
 eta_thrust = 0.65 # thrust efficiency
@@ -89,33 +137,56 @@ C_AtomicMass = 12.00 * 1.67 * 10**(-27) # amu -> kg, Carbon-12
 
 # Ground State ionization energy
 He_GroundIonization = 24.59 # [eV]
+# He_GroundIonization = 0.0 # [eV]
 Li_GroundIonization = 5.39 # [eV]
 C_GroundIonization = 11.26 # [eV]
 
 # Energy levels are given in units of [1/cm] from NIST - multiply by h*c to turn into energy
 # These are the \beta_{k} from Jahn Ch.6
-He_Neutral_Excitation = 159856.0 # 1s^2 -> 1s2s and J = 0 -> 1
-Li_Neutral_Excitation = 14903.6 # 2s -> 2p and J = 1/2 -> 1/2
-C_Neutral_Excitation = 33735.2 # 2s^2 2p^2 -> 2s2p^3 and J = 0 -> 2
+# He_Neutral_Excitation = 159856.0 # 1s^2 -> 1s2s and J = 0 -> 1
+# Li_Neutral_Excitation = 14903.6 # 2s -> 2p and J = 1/2 -> 1/2
+# C_Neutral_Excitation = 33735.2 # 2s^2 2p^2 -> 2s2p^3 and J = 0 -> 2
+He_Neutral_Excitation = 0.0 # 1s^2 -> 1s2s and J = 0 -> 1
+Li_Neutral_Excitation = 0.0 # 2s -> 2p and J = 1/2 -> 1/2
+C_Neutral_Excitation = 0.0 # 2s^2 2p^2 -> 2s2p^3 and J = 0 -> 2
 
 # There are the \beta_{m} from Jahn Ch. 6
-He_SingIon_Excitation = 329179.3 # 1s -> 2p and J = 1/2 -> 1/2
-Li_SingIon_Excitation = 476034.2 # 1s^2 -> 1s2s and J = 0 -> 0
-C_SingIon_Excitation =  43003.3 # 2s^2 2p -> 2s 2p^2 and J = 1/2 -> 1/2
+# He_SingIon_Excitation = 329179.3 # 1s -> 2p and J = 1/2 -> 1/2
+# Li_SingIon_Excitation = 476034.2 # 1s^2 -> 1s2s and J = 0 -> 0
+# C_SingIon_Excitation =  43003.3 # 2s^2 2p -> 2s 2p^2 and J = 1/2 -> 1/2
+He_SingIon_Excitation = 0.0 # 1s -> 2p and J = 1/2 -> 1/2
+Li_SingIon_Excitation = 0.0 # 1s^2 -> 1s2s and J = 0 -> 0
+C_SingIon_Excitation =  0.0 # 2s^2 2p -> 2s 2p^2 and J = 1/2 -> 1/2
 
 # Collect numbers together to do computation in a for loop
+Elements = ["Helium", "Lithium", "Carbon"]
 N0 = [1.0/He_AtomicMass, 1.0/Li_AtomicMass, 1.0/C_AtomicMass] # [1/kg]
-NeutralExcitation = 10**(2)*h*c*[He_Neutral_Excitation, Li_Neutral_Excitation, C_Neutral_Excitation] # [J], 1/cm -> 1/m and then to energy
-SingIonExcitation = 10**(2)*h*c[He_SingIon_Excitation, Li_SingIon_Excitation, C_SingIon_Excitation] # [J], 1/cm -> 1/m and then to energy
-GroundIonization = 1.0/Joules_to_eV)*[He_GroundIonization, Li_GroundIonization, C_GroundIonization] # [eV] - [J]
+NeutralExcitation = [He_Neutral_Excitation, Li_Neutral_Excitation, C_Neutral_Excitation]
+SingIonExcitation = [He_SingIon_Excitation, Li_SingIon_Excitation, C_SingIon_Excitation]
+GroundIonization = [He_GroundIonization, Li_GroundIonization, C_GroundIonization]
 
 xi = np.linspace(0.0,1.0,25) # What proportion of the flow's internal energy is lost
-Tc = np.empty((xi.shape[0],len(N0)) # Chamber temperature for a given xi and particular atom
+Tc = np.empty((xi.shape[0],len(N0))) # Chamber temperature for a given xi and particular atom
 
-for element in N0:
-    for xidx in xi.shape[0]:
-        Tc[xidx] = (2.0/(3.0*k_b*alpha_p))*((g0*Isp_5)**(2)/(2.0*N0) - (1.0 - xi[xidx])*)
+TcProblem5Fig = plt.figure()
 
+for eidx in np.arange(len(Elements)):
+    NeutralExcitation[eidx] = 10**(2)*h*c*NeutralExcitation[eidx] # [1/cm]-> [J]
+    SingIonExcitation[eidx] = 10**(2)*h*c*SingIonExcitation[eidx] # [1/cm] -> [J]
+    GroundIonization[eidx] = (1.0/Joules_to_eV)*GroundIonization[eidx] # [eV] -> [J]
+    for xidx in np.arange(xi.shape[0]):
+        e_Frozen = alpha_1*NeutralExcitation[eidx] + alpha_1p*SingIonExcitation[eidx] + alpha_1p*GroundIonization[eidx]
+        Tc[xidx,eidx] = (2.0/(3.0*k_b*alpha_p))*((g0*Isp_5)**(2)/(2.0*N0[eidx]) - (1.0 - xi[xidx])*e_Frozen)
+    plt.plot(xi,Tc[:,eidx],label=Elements[eidx])
+    print("For %s at xi = %f, Tc = %f [K]" %(Elements[eidx],xi[0],Tc[0,eidx]))
+    print("For %s at xi = %f, Tc = %f [K]" %(Elements[eidx],xi[xi.shape[0]-1],Tc[xi.shape[0]-1,eidx]))
+
+plt.xlabel('Frozen Flow loss parameter $\\xi$')
+plt.ylabel('Chamber Temperature, $T_{c}$ [K]')
+plt.title('$T_{c}$ vs. $\\xi$')
+plt.legend()
+
+plt.show()
 # Isp_5 = 1000.0 # [sec]
 # P_5 = 1.0 # [atm]
 #
